51db8a2251e0bb10930020845faf167310908baa
Devon Fruck <devon.fruck@hotmail.com>
2022-05-27 17:07:18 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+On Time is a web app used to keep track of things to do in a day, with friends!
+See your friend's progress and check off tasks as you complete them!
---
+++ backend/routes/users.js
@@ -0,0 +1,66 @@
+var express = require('express');
+//const client = require('pg/lib/native/client');
+var router = express.Router();
+const { Client } = require('pg')
+
+var client = new Client({
+  host: 'host.docker.internal',
+  port: 49153,
+  user: 'postgres',
+  password: 'postgrespw',
+})
+
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+})
+
+
+
+/* GET users tasks. */
+router.get('/', function(req, response, next) {
+
+    let groupedData = {};
+    client.query('SELECT * FROM public.tasks', (err, res) => {
+        if (err) throw err
+
+        res.rows.forEach(task => {
+            if (!(task.name in groupedData)) {
+                groupedData[task.name] = [];
+            }
+            groupedData[task.name].push({title: task.task_name, isComplete: task.is_complete})
+        
+        })
+        response.send(groupedData)
+    })
+
+    //console.log(groupedData)
+    //response.send(res.rows)
+
+    //res.send('respond with a resource');
+});
+
+/* PUT user task. */
+router.put('/', function(req, response, next) {
+
+    var groupedData;
+    client.query(`SELECT * FROM public.tasks`, (err, res) => {
+        if (err) throw err
+        //console.log(res.rows)
+
+        res.rows.forEach(task => {
+            groupedData[task.name].append({title: task.task_name, isComplete: task.is_complete})
+        })
+
+        //console.log(groupedData)
+
+        //response.send(res.rows)
+
+    })
+    res.send('respond with a resource');
+});
+
+module.exports = router;
