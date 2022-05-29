b169d0ca5848ad6be2a35c8448eca7cf2adfe338
Devon Fruck <devon.fruck@hotmail.com>
2022-05-29 11:12:15 -0400

    Added icons for editing tasks and made the task lists look nicer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,111 +1,178 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import SendIcon from "@mui/icons-material/Send";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import EditIcon from '@mui/icons-material/Edit';
 
-import { TextField, Button } from '@material-ui/core';
+import {
+  TextField,
+  Button,
+  Checkbox,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  Paper
+} from "@material-ui/core";
+
+function AvailableButtons({ editMode, setEditMode, enabled }) {
+    if(!enabled) return;
+    if(editMode) {
+        return(
+            <span className="title-btns">
+                <Button onClick={() => setEditMode(false)}>
+                    <CancelIcon sx={{ color: "crimson" }}/>
+                </Button>
+                <Button onClick={() => setEditMode(false)}>
+                    <CheckCircleIcon color='success'/>
+                </Button>
+            </span>
+        )
+    } else {
+        return(
+            <span className="title-btns">
+                <Button onClick={() => setEditMode(true)}>
+                    <EditIcon/>
+                </Button>
+            </span>
+        )
+    }
+}
 
 function App() {
-
-    let data =[
+  let data = [
+    {
+      name: "Devon",
+      id: 12,
+      tasks: [
         {
-            name: 'Devon',
-            id: 12,
-            tasks: [
-                {
-                    taskName: "mow the grass",
-                    isComplete: false,
-                    order: 1
-                },
-                {
-                    taskName: "eat some tea",
-                    isComplete: true,
-                    order: 3
-                },
-                {
-                    taskName: "cry",
-                    isComplete: false,
-                    order: 2
-                }
-            ]
+          taskName: "mow the grass",
+          isComplete: false,
+          order: 1,
         },
         {
-            name: "Johnny",
-            id: 52,
-            tasks: [
-                {
-                    taskName: "woohoo ur dad",
-                    isComplete: false
-                },
-                {
-                    taskName: "sleep",
-                    isComplete: true
-                },
-                {
-                    taskName: "snore",
-                    isComplete: false
-                }
-            ]
-        }
-    ]
+          taskName: "eat some tea",
+          isComplete: true,
+          order: 3,
+        },
+        {
+          taskName: "cry",
+          isComplete: false,
+          order: 2,
+        },
+      ],
+    },
+    {
+      name: "Johnny",
+      id: 52,
+      tasks: [
+        {
+          taskName: "woohoo ur dad",
+          isComplete: false,
+        },
+        {
+          taskName: "sleep",
+          isComplete: true,
+        },
+        {
+          taskName: "snore",
+          isComplete: false,
+        },
+      ],
+    },
+  ];
 
-    const [personalTasks, setPersonalTasks] = useState({});
-    const [myTasks, setMyTasks] = useState([]);
-    const [signedInUser, setSignedInUser] = useState(12);
-    const [forceRefresh, setForceRefresh] = useState({});
+  const [allTasks, setAllTasks] = useState([]);
 
-    useEffect(() => {
-        console.log('refreshing...')
-        console.log(myTasks)
-    }, [forceRefresh])
+  //const [myTasks, setMyTasks] = useState([]);
+  const [editModeTasks, setEditModeTasks] = useState([]);
+  const [signedInUser, setSignedInUser] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [newTask, setNewTask] =  useState('');
 
-    useEffect(() => {
-        setMyTasks(data);
-    }, [])
+  // TODO: fetch the initial data from backend
+  useEffect(() => {
+    // const fetchData = async () => {
+    //   await axios.get("http://localhost:3001/users")
+    //     .then( res => {
+    //       //setAllTasks(res)
+    //       console.log(res)
+    //     })
+    // }
 
+    //fetchData()
 
-    return (
-        <div className="App">
-            {
-                myTasks.map((user, index) => {
-                    return (
-                        <div>
-                            <p>
-                                {user.name}
-                            </p>
-                            {          
-                            Array.isArray(user.tasks) ?
-                            user.tasks.map( (task) => {
-                                    return (
-                                        <p>
-                                            {task.taskName}
-                                        </p>
-                                    );
-                            }) : null }
-                            {
-                             user.id === signedInUser &&
-                                <span>
-                                    <TextField id="filled-basic" label="Filled" variant="filled" />
-                                        <Button onClick={() => {
-                                            const newState = myTasks.map( (user) => {
-                                                if(user.id === 12) {
-                                                    user.tasks.push( {taskName: 'Wahoo', isComplete: false, order: 4})
-                                                }
-                                                return user
-                                            })
-                                            setMyTasks(newState)
-                                        }}>
-                                            Send
-                                        </Button>
-                                </span>
-                            }
-                        </div>
-                    )
-                    
-                })
-            }
-            
-        </div>
-    );
+    setAllTasks(data);
+    setSignedInUser(12);
+  }, []);
+
+  return (
+    <div className="App">
+      {allTasks.map((user) => {
+        return (
+          <Paper elevation={4} className="user-card">
+            <div className="name-bar">
+                {user.name}
+                <AvailableButtons editMode={editMode} setEditMode={setEditMode} enabled={user.id === signedInUser}/>
+            </div>
+
+            <Table>
+              <TableBody>
+                {Array.isArray(user.tasks)
+                  ? user.tasks.map((task) => {
+                      return (
+                        <TableRow align="left" className="task">
+                          <TableCell>{task.taskName}</TableCell>
+                          <TableCell align="right" className="checkbox">
+                            <Checkbox />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
+                  : null}
+              </TableBody>
+            </Table>
+            {user.id === signedInUser && (
+              <span className="task-input">
+                <TextField
+                  value={newTask}
+                  id="filled-basic"
+                  label="Enter new task"
+                  variant="filled"
+                  size="small"
+                  multiline
+                  maxRows={3}
+                  fullWidth={true}
+                  onChange={ (event) => setNewTask(event.target.value)}
+                />
+                <Button
+                  disabled={!newTask.trim()}
+                  onClick={() => {
+                    const newState = allTasks.map((user) => {
+                      if (user.id === 12) {
+                        user.tasks.push({
+                          taskName: newTask,
+                          isComplete: false,
+                          order: 4,
+                        });
+                      }
+                      return user;
+                    });
+                    setAllTasks(newState);
+                    setNewTask('');
+                  }}
+                >
+                  <SendIcon color={newTask.trim() ? "primary" : "default"} />
+                </Button>
+              </span>
+            )}
+          </Paper>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
