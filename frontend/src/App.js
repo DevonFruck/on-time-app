c0b29dea3cf13a49618671b422a9d479d9bb3b2a
Devon Fruck <devon.fruck@hotmail.com>
2022-06-21 18:09:04 -0400

    ran prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { LoginModal, UserCard, getAllTasks, addTask, removeTask, updateTaskStatus } from './Components'
+import {
+  LoginModal,
+  UserCard,
+  getAllTasks,
+  addTask,
+  removeTask,
+  updateTaskStatus,
+} from "./Components";
 import "./App.css";
-
 
 function App() {
   const [allTasks, setAllTasks] = useState({});
   const [signedInUser, setSignedInUser] = useState(null);
   const [userDisplayName, setUserDisplayName] = useState(null);
-
 
   const fetchData = async () => {
     let tasks = await getAllTasks();
@@ -17,23 +22,23 @@ function App() {
     Adds the logged in user to the list if not already
     This makes it so their task card still shows up
     although their task list is empty */
-    if(!tasks[signedInUser]) {
+    if (!tasks[signedInUser]) {
       tasks[signedInUser] = {
         name: userDisplayName,
-        tasks: []
-      }
+        tasks: [],
+      };
     }
 
     setAllTasks(tasks);
-  }
+  };
 
   async function handleAddTask(userId, newTaskName, displayName) {
     let newState = allTasks;
-                      
+
     const reqBody = {
       taskName: newTaskName,
-      userId: userId
-    }
+      userId: userId,
+    };
     const newTaskId = await addTask(reqBody);
 
     newState[userId].tasks.push({
@@ -41,65 +46,70 @@ function App() {
       title: newTaskName,
       isComplete: false,
       //order: 4,
-    })
-  
+    });
+
     setAllTasks(newState);
   }
-  
+
   async function handleDeleteTask(userId, taskId) {
     const reqBody = {
       userId: userId,
-      taskId: taskId
-    }
+      taskId: taskId,
+    };
     await removeTask(reqBody);
-  
+
     let newUserData = JSON.parse(JSON.stringify(allTasks));
-  
-    newUserData[userId].tasks = newUserData[userId].tasks
-      .filter(item => item.taskId !== taskId);
-    
+
+    newUserData[userId].tasks = newUserData[userId].tasks.filter(
+      (item) => item.taskId !== taskId
+    );
+
     setAllTasks(newUserData);
   }
-  
+
   async function handleStatusChange(userId, taskId, status) {
     const reqBody = {
       userId: userId,
       taskId: taskId,
-      newStatus: status
-    }
-  
-    await axios.post("http://localhost:3001/task/remove", reqBody)
-      .then( res => {
+      newStatus: status,
+    };
+
+    await axios
+      .post("http://localhost:3001/task/remove", reqBody)
+      .then((res) => {
         return res.data;
-      })
+      });
   }
 
   useEffect(() => {
     if (signedInUser !== null) {
-      fetchData()
+      fetchData();
     }
   }, [signedInUser]);
-
 
   return (
     <div className="App">
       {signedInUser &&
         Object?.entries(allTasks)?.map((user) => {
-        const userId = parseInt(user[0]);
-        const userData = user[1];
+          const userId = parseInt(user[0]);
+          const userData = user[1];
 
-        return (
-          <UserCard 
-            userData={userData}
-            userId={userId}
-            hasInput={userId === signedInUser}
-            handleDeleteTask={handleDeleteTask}
-            handleAddTask={handleAddTask}
-            handleStatusChange={handleStatusChange}
-          />
-        );
-      })}
-      <LoginModal signedInUser={signedInUser} setSignedInUser={setSignedInUser} setUserDisplayName={setUserDisplayName} />
+          return (
+            <UserCard
+              userData={userData}
+              userId={userId}
+              hasInput={userId === signedInUser}
+              handleDeleteTask={handleDeleteTask}
+              handleAddTask={handleAddTask}
+              handleStatusChange={handleStatusChange}
+            />
+          );
+        })}
+      <LoginModal
+        signedInUser={signedInUser}
+        setSignedInUser={setSignedInUser}
+        setUserDisplayName={setUserDisplayName}
+      />
     </div>
   );
 }
--- frontend/src/Components/UserCard/UserCard.js
+++ frontend/src/Components/UserCard/UserCard.js
@@ -1,82 +1,97 @@
 import React, { useState } from "react";
-import { Table, TableRow , TableCell, TableBody, Button, Checkbox, TextField, Paper } from '@material-ui/core';
-import EditIcon from '@mui/icons-material/Edit';
+import {
+  Table,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+  Checkbox,
+  TextField,
+  Paper,
+} from "@material-ui/core";
+import EditIcon from "@mui/icons-material/Edit";
 import SendIcon from "@mui/icons-material/Send";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
+export function UserCard({
+  userData,
+  userId,
+  hasInput,
+  handleDeleteTask,
+  handleAddTask,
+  handleStatusChange,
+}) {
+  const [editMode, setEditMode] = useState(false);
+  const [taskInput, setTaskInput] = useState("");
 
-export function UserCard({ userData, userId, hasInput, handleDeleteTask, handleAddTask, handleStatusChange }) {
-
-    const [editMode, setEditMode] = useState(false);
-    const [taskInput, setTaskInput] = useState('');
-
-    return (
-        <Paper elevation={4} className="user-card">
-            <div className="name-bar">
-                {userData.name}
-                <span className="title-btns">
-                    {
-                        editMode ?
-                            <Button onClick={ () => setEditMode(false) }>
-                                <CheckCircleIcon color='success'/>
-                            </Button>
-                        :
-                            <Button onClick={() => setEditMode(true)}>
-                                <EditIcon/>
-                            </Button>
-                    }
-                </span>
-            </div>
-        <Table>
-            <TableBody>
-                {userData?.tasks?.map((task) => {
-                    return (
-                        <TableRow align="left" className="task">
-                            <TableCell>{task.title}</TableCell>
-                            <TableCell align="right" className="checkbox">
-                                {editMode ?
-                                    <Button
-                                        onClick={ async () => handleDeleteTask(userId, task.taskId) }
-                                    >
-                                    <DeleteForeverIcon sx={{ color: "crimson" }}/>
-                                    </Button>
-                                    :
-                                    <Checkbox />
-                                }
-                            </TableCell>
-                        </TableRow>
-                    );
-                })}
-            </TableBody>
-        </Table>
-
-        {hasInput && (
-            <span className="task-input">
-            <TextField
-                value={taskInput}
-                id="filled-basic"
-                label="Enter new task"
-                variant="filled"
-                size="small"
-                multiline
-                maxRows={3}
-                fullWidth={true}
-                onChange={ (event) => {
-                    setTaskInput(event.target.value)
-                }}
-            />
-            <Button
-                disabled={!taskInput.trim()}
-                onClick={async () => {
-                    await handleAddTask(userId, taskInput.trim());
-                    setTaskInput('');
-                }}
-            >
-                <SendIcon color={taskInput.trim() ? "primary" : "default"} />
+  return (
+    <Paper elevation={4} className="user-card">
+      <div className="name-bar">
+        {userData.name}
+        <span className="title-btns">
+          {editMode ? (
+            <Button onClick={() => setEditMode(false)}>
+              <CheckCircleIcon color="success" />
             </Button>
-            </span>
-        )}
+          ) : (
+            <Button onClick={() => setEditMode(true)}>
+              <EditIcon />
+            </Button>
+          )}
+        </span>
+      </div>
+      <Table>
+        <TableBody>
+          {userData?.tasks?.map((task) => {
+            return (
+              <TableRow align="left" className="task">
+                <TableCell>{task.title}</TableCell>
+                <TableCell align="right" className="checkbox">
+                  {editMode ? (
+                    <Button
+                      onClick={async () =>
+                        handleDeleteTask(userId, task.taskId)
+                      }
+                    >
+                      <DeleteForeverIcon sx={{ color: "crimson" }} />
+                    </Button>
+                  ) : (
+                    <Checkbox />
+                  )}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+
+      {hasInput && (
+        <span className="task-input">
+          <TextField
+            value={taskInput}
+            id="filled-basic"
+            label="Enter new task"
+            variant="filled"
+            size="small"
+            multiline
+            maxRows={3}
+            fullWidth={true}
+            onChange={(event) => {
+              setTaskInput(event.target.value);
+            }}
+          />
+          <Button
+            disabled={!taskInput.trim()}
+            onClick={async () => {
+              await handleAddTask(userId, taskInput.trim());
+              setTaskInput("");
+            }}
+          >
+            <SendIcon color={taskInput.trim() ? "primary" : "default"} />
+          </Button>
+        </span>
+      )}
     </Paper>
-    )
+  );
 }
